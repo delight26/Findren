89db616606c3f23a5a90cde21088dd505c6f2b6d
HyominSeo <mina_seo420@hotmail.com>
2016-04-05 10:13:29 +0900

    1.SQL문 (게시판, 멤버) 추가 2. 게시판 dao, service, controller 주석 추가

--- src/main/java/com/findren/homepage/dao/NoticeDao.java
+++ src/main/java/com/findren/homepage/dao/NoticeDao.java
@@ -6,23 +6,33 @@ import com.findren.homepage.domain.NoticeBoard;
 
 public interface NoticeDao {
 
+	//게시판 글 수 
 	public Integer getNoticeBoardCount();
 	
+	//리스트 페이징
 	public List<NoticeBoard> getNoticeBoardList(int startRow, int PAGE_SIZE);
 	
+	//글 입력
 	public void insertNoticeBoard(NoticeBoard nb);
 	
+	//조회수
 	public void noticeWatchUpdate(int watch, int no);
 	
+	//글내용
 	public NoticeBoard noticeContent(int no);
 	
+	//다음 글 번호
 	public Integer noticeNextNo(int no);
 	
+	//이전글 번호
 	public Integer noticePreNo(int no);
 	
+	//삭제
 	public void noticeDelete(int no);
 	
+	//업데이트
 	public void updateNoticeBoard(NoticeBoard nb);
 	
+	//게시글 최대 수
 	public Integer maxNum();
 }
--- src/main/java/com/findren/homepage/service/NoticeService.java
+++ src/main/java/com/findren/homepage/service/NoticeService.java
@@ -9,19 +9,27 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 public interface NoticeService {
 
+	//리스트
 	public void noticeList(HttpServletRequest request);
 
+	//글쓰기결과
 	public void noticeWriteResult(MultipartHttpServletRequest request, String path, HttpSession session) throws IllegalStateException, IOException;
 	
+	//글내용
 	public void noticeContent(HttpServletRequest request);
 	
+	//다음글
 	public void noticeNext(HttpServletRequest request);
 	
+	//이전글
 	public void noticePre(HttpServletRequest request);
 	
+	//삭제
 	public void noticeDelete(HttpServletRequest request);
 	
+	//업데이트
 	public void noticeUpdate(HttpServletRequest request);
 	
+	//업데이트 결과
 	public void noticeUpdateResult(MultipartHttpServletRequest request, String path, HttpSession session) throws IllegalStateException, IOException;
 }
